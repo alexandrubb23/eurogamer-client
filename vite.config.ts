23760e0eb5d67ec6f03de81aefe979c13adc311a
Barbulescu Alexandru <alex_bb23@yahoo.co.uk>
2023-12-12 15:32:47 +0200

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: '/src',
-      },
-    ],
-  },
-  server: {
-    port: 3000, // TODO: Use nev variable
-  },
-});
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+    resolve: {
+      alias: [
+        {
+          find: '@',
+          replacement: '/src',
+        },
+      ],
+    },
+    server: {
+      port: parseInt(process.env.VITE_APP_PORT),
+    },
+  });
+};
